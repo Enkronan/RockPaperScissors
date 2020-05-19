7be1f0db637af3f2b1becf6d1e2c563883901e15
enkronan <robin@tempura.se>
2020-05-19 18:05:00 +0200

    type: refactor
    added a game function which will create a scope for all the inner functions that control the game

--- app.js
+++ app.js
@@ -2,5 +2,22 @@ const game = () => {
     let pScore = 0;
     let cScore = 0;
 
-    
-}
+    const startGame = () =>{
+        const playButton = document.querySelector(".intro button");
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
+        console.log(introScreen);
+
+        playButton.addEventListener('click', () => {
+            introScreen.classList.add('fadeOut');
+            match.classList.add('fadeIn');
+        });
+    };
+
+
+    //call inner functions
+    startGame();
+};
+
+game();
