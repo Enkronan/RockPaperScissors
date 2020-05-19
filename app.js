0e58f68f8863c3955344d5ad2e833d533f0ad284
enkronan <robin@tempura.se>
2020-05-19 18:32:31 +0200

    type: refactor
    added function for comparing player hands

--- app.js
+++ app.js
@@ -23,13 +23,53 @@ const game = () => {
 
         options.forEach(option => {
             option.addEventListener('click', function(){
+                //generate computer option
                 const randomNumber = Math.floor(Math.random() * 3);
                 const computerChoice = computerOptions[randomNumber];
-            
-            })
-        });
+                //compare hands
 
-        
+            });
+        });
+    };
+
+    const compareHands = (playerChoice, computerChoice) => {
+        const winner = document.querySelector('.winner');
+        //check for tie
+        if(playerChoice === computerChoice){
+            winner.textContent = "It is a tie";
+            return;
+        }
+        //check for rock
+        if(playerChoice === 'rock'){
+            if(computerChoice === 'scissors'){
+                winner.textContent = "Player wins";
+                return;
+            }else{
+                winner.textContent = "Computer wins";
+                return;
+            }
+        }
+        //check for scissors
+        if(playerChoice === 'scissors'){
+            if(computerChoice === 'paper'){
+                winner.textContent = "Player wins";
+                return;
+            }else{
+                winner.textContent = "Computer wins";
+                return;
+            }
+        }
+        //check for paper
+        if(playerChoice === 'paper'){
+            if(computerChoice === 'rock'){
+                winner.textContent = "Player wins";
+                return;
+            }else{
+                winner.textContent = "Computer wins";
+                return;
+            }
+        }
+
     }
 
 
